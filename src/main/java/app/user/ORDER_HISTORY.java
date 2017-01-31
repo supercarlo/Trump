617fef360df5495f2dcb2e6a82cacc5da0f592fc
wrkettlitz <wrkettlitz@hotmail.nl>
2017-01-31 10:15:56 +0100

    test12345

--- src/main/java/app/user/ORDER_HISTORY.java
+++ src/main/java/app/user/ORDER_HISTORY.java
@@ -2,8 +2,11 @@ package app.user;
 
 import app.DBC;
 
+import java.sql.Array;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wrket on 26-Jan-17.
@@ -11,25 +14,64 @@ import java.sql.Statement;
 public class ORDER_HISTORY {
 
 
-    public String ShowOrderHistory(String usernamecustomer) {
+    public List GetOrderIDs(String usernamecustomer) {
         DBC databasePandaShop = new DBC();
         Statement stat = databasePandaShop.Connection();
-        String returnstatement = "";
+        List myList = new ArrayList();
         try {
-            String query = ("SELECT oh.* from orders_history oh, orders_history_customer_junctiontable ohcj where ohcj.usernamecustomer =" + usernamecustomer + "AND ohcj.orderid = oh.orderid;");
+            String query = ("Select orderid from orders_history_customer_junctiontable where usernamecustomer = " + usernamecustomer + ";" );
             ResultSet rs = stat.executeQuery(query);
             while (rs.next()) {
-                returnstatement+= Integer.toString(rs.getInt("orderid")) + "     ";
-                returnstatement+= Integer.toString(rs.getInt("poduct_id")) + "     ";
-                returnstatement+= Double.toString(rs.getDouble("price")) + "        ";
-                returnstatement+= Integer.toString(rs.getInt("quantity")) + "<br>";
+                myList.add(rs.getInt("orderid"));
             }
             rs.close();
-            return returnstatement;
+            return myList;
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
-            return "";
+            return myList;
         }
+    }
+    public ArrayList ShowOrderHistory(List orderidlist) {
+        DBC databasePandaShop = new DBC();
+        Statement stat = databasePandaShop.Connection();
+        ArrayList myList = new ArrayList();
+        try {
+            for (int i = 0; i < orderidlist.size(); i++) {
+                String query = ("SELECT * from orders_history where orderid = " + orderidlist.get(i));
+                ResultSet rs = stat.executeQuery(query);
+                while (rs.next()) {
+                    ArrayList myList2 = new ArrayList();
+                    myList2.add(Integer.toString(rs.getInt("orderid")));
+                    myList2.add(Integer.toString(rs.getInt("poduct_id")));
+                    myList2.add(Double.toString(rs.getDouble("price")));
+                    myList2.add(Integer.toString(rs.getInt("quantity")));
+                    myList.add(myList2);
+                }
+                rs.close();
+
+            }
+            return myList;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+            return myList;
+        }
+    }
+    public String GenerateHTMLTable(List orderlist) {
+        String Header = "<table>\n" +
+                "  <tr>\n" +
+                "    <th>Product Id</th>\n" +
+                "    <th>Product Name</th>\n" +
+                "    <th>Price</th>\n" +
+                "    <th>Quantity</th>\n" +
+                "    <th>Total Price</th>\n" +
+                "  </tr>";
+
+        
+
+
+
+
     }
 }
