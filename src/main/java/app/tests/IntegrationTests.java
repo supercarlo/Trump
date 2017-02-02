1e9ada264f4dd120ada8782c23d304dd0c77f22e
Sam0174 <aardappel1234567890@gmail.com>
2017-02-02 15:49:42 +0100

    Testen is klaar

--- src/main/java/app/tests/IntegrationTests.java
+++ src/main/java/app/tests/IntegrationTests.java
@@ -2,9 +2,9 @@ package app.tests;
 
 import app.product.ProductDao;
 import app.product.Products;
+import app.shoppingcart.shoppingcartDAO;
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Samuel on 02-02-17.
@@ -13,20 +13,22 @@ public class IntegrationTests {
 
     @Test
     public void loginPost() {
-//        LoginController tester = new LoginController();
-        Boolean name=true;
-        Boolean name2=false;
-        String name3="Pieter";
-        String name4="class app.product.ProductDao";
-        String name5="1";
-        // assert statements
-//        String result3 = String.valueOf(ProductDao.class);
-//        Assert.assertEquals(name4, result3);
-        ProductDao.products.add(new Products(name5, name3, name3, name3, name3, name3));
+        String name1="1";
+        String name2="Pieter";
+        ProductDao.products.add(new Products(name1, name2, name2, name2, name2, name2));
         Products result2 = ProductDao.getProductByID("1");
-        //assertEquals(new Products(name5, name3, name3, name3, name3, name3), result2);
-        assertEquals(new Products(name5, name3, name3, name3, name3, name3), result2);
+        Assert.assertEquals(new Products(name1, name2, name2, name2, name2, name2), result2);
 
     }
+
+    @Test
+    public void shoppingCartTest() {
+        String name5="onno";
+        String result3 = shoppingcartDAO.addProductToCart(name5);
+        Assert.assertEquals(name5, result3);
+
+    }
+
+
 
 }
--- src/main/java/app/tests/UnitTests.java
+++ src/main/java/app/tests/UnitTests.java
@@ -4,7 +4,6 @@ package app.tests;
  * Created by Samuel on 23-01-17.
  */
 
-import app.login.RegisterController;
 import app.login.User;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -15,39 +14,6 @@ import static org.junit.Assert.assertEquals;
 @FixMethodOrder(MethodSorters.JVM)
 public class UnitTests {
 
-    //Testing the testing method
-    @Test
-    public void nameTester() {
-        RegisterController tester = new RegisterController();
-        Boolean name=true;
-        Boolean name2=false;
-        String name3="Pieter";
-        // assert statements
-       Boolean result2 = tester.multiplyString(name);
-        assertEquals(name, result2);
-        Boolean result3 = tester.multiplyString(name);
-        assertEquals(name, result3);
-    }
-
-//    @Test
-//    public void loginPost() {
-////        LoginController tester = new LoginController();
-//        Boolean name=true;
-//        Boolean name2=false;
-//        String name3="Pieter";
-//        String name4="class app.product.ProductDao";
-//        String name5="1";
-//        // assert statements
-////        String result3 = String.valueOf(ProductDao.class);
-////        Assert.assertEquals(name4, result3);
-//        ProductDao.products.add(new Products(name5, name3, name3, name3, name3, name3));
-//        Products result2 = ProductDao.getProductByID("1");
-//        //assertEquals(new Products(name5, name3, name3, name3, name3, name3), result2);
-//        assertEquals(new Products(name5, name3, name3, name3, name3, name3), result2);
-////        Boolean result3 = tester.handleLoginPost(name);
-////        Assert.assertEquals(name, result3);
-//    }
-
     //Testing the adress queries
     @Test
     public void createAdressTester() {
@@ -56,7 +22,6 @@ public class UnitTests {
         String name2="Toos";
         String name3="Pieter";
         String name4="city";
-        // assert statements
         String result = tester.createAdress(name, name2, name3, name, name3);
         assertEquals("insert into adress(adressid, city, postalcode ,street, housenumber) values('"+name+"', '"+name2+"', '"+name3+"', '"+name+"', '"+name3+"');", result);
         String result1 = tester.alterAdress(name4, name2, name3);
@@ -89,7 +54,6 @@ public class UnitTests {
     @Test
     public void wishlistTester() {
         User tester = new User();
-        String name="02-12-2016";
         String name2="Toos";
         String name3="2";
         // assert statements
